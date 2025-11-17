7ac221e8d23fe67f0de13f6ed2cb43cc7e3f4a2d
lex-desktop <lex-desktop@local>
2025-11-17 18:55:51 +0200

    add EF migration for timestamp index

---
+++ Migrations/20251117165013_AddTimestampIndex.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projectlexagon_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimestampIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Temperature",
+                table: "SensorData",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Humidity",
+                table: "SensorData",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorData_Timestamp",
+                table: "SensorData",
+                column: "Timestamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SensorData_Timestamp",
+                table: "SensorData");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Temperature",
+                table: "SensorData",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Humidity",
+                table: "SensorData",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
